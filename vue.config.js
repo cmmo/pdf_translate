fd85d2bcad67f0f47b0f99ff0a90c3dc46c46dc7
Dio Lee <dio.lee@gmail.com>
2021-03-29 01:40:34 +0000

    Start

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+  chainWebpack: config => {
+    config.performance
+      .maxEntrypointSize(700000)
+      .maxAssetSize(400000)
+  }
+}
