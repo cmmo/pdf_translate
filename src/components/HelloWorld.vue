6132a73221cda030a976f4cd99c4fcaecb30dcb2
Dio Lee <dio.lee@gmail.com>
2021-05-01 09:15:22 +0900

    check if file is new

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,6 +10,17 @@
         <v-btn color="primary" @click="onUpload">Upload</v-btn>
       </v-col>
     </v-row>
+    <v-row class="text-center">
+      <v-col class="mb-5" cols="6">
+        <v-textarea
+          name="Translated Result"
+          filled
+          label="Translat to Japanese"
+          auto-grow
+          v-model="text"
+        ></v-textarea>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -26,20 +37,6 @@ export default {
   }),
 
   methods: {
-    onUploadBak() {
-      console.log(this.file);
-      // Credit: https://stackoverflow.com/a/754398/52160
-      let reader = new FileReader();
-      reader.readAsDataURL(this.file);
-      reader.onload = (evt) => {
-        this.text = evt.target.result;
-        console.log(this.text);
-      };
-      reader.onerror = (evt) => {
-        console.error(evt);
-      };
-    },
-
     async onUpload() {
       const storageRef = storage.ref(`beforeTranslate.pdf`);
       console.log(storageRef);
@@ -47,16 +44,44 @@ export default {
         console.log("Uploaded file!");
       });
 
+      //check time
+      let timestamp = Math.round(+new Date() / 1000);
+      console.log(timestamp);
+
+      let file_updatetime = timestamp - 1; // initial with past time
+
+      while (file_updatetime < timestamp) {
+        await storage
+          .ref("afterTranslate.txt")
+          .getMetadata()
+          .then((metadata) => {
+            let date = new Date(metadata.updated);
+            file_updatetime = Math.round(date.getTime() / 1000);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+
+        if (file_updatetime < timestamp) {
+          await new Promise(resolve => setTimeout(resolve, 1000)) // 1秒待つ
+          console.log("Wait 1 second")
+        }
+      }
+
+      console.log("New file translated");
+      //check time end
+
       storage
         .ref("afterTranslate.txt")
         .getDownloadURL()
         .then((url) => {
           var xhr = new XMLHttpRequest();
-          xhr.withCredentials = false; 
+          xhr.withCredentials = false;
           xhr.responseType = "text";
           xhr.onload = () => {
             var text = xhr.response;
             console.log(text);
+            this.text = text
           };
           xhr.open("GET", url);
           xhr.send();
